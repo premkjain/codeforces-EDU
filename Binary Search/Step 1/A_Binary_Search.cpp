dbd1cda304adb9b3027b271c4f5d81f78f9aaad0
Prem Kamal Jain <108341999+premkjain@users.noreply.github.com>
2022-10-07 01:16:55 +0530

    Create A_Binary_Search.cpp

---
+++ Binary Search/Step 1/A_Binary_Search.cpp
@@ -0,0 +1,44 @@
+/* ____                       _  __                     _       _       _       
+  |  _ \ _ __ ___ _ __ ___   | |/ /__ _ _ __ ___   __ _| |     | | __ _(_)_ __  
+  | |_) | '__/ _ \ '_ ` _ \  | ' // _` | '_ ` _ \ / _` | |  _  | |/ _` | | '_ \ 
+  |  __/| | |  __/ | | | | | | . \ (_| | | | | | | (_| | | | |_| | (_| | | | | |
+  |_|   |_|  \___|_| |_| |_| |_|\_\__,_|_| |_| |_|\__,_|_|  \___/ \__,_|_|_| |_|
+*/                                                                            
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define debug(x); cout<<#x<<" "<<x<<endl;
+ll hell=1e9+7;
+
+void solve()
+{
+    ll n,k;
+    cin>>n>>k;
+    vector<ll> v(n);
+    for(ll i=0;i<n;i++){
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    for(ll i=0;i<k;i++){
+        ll x;
+        cin>>x;
+        if(binary_search(v.begin(),v.end(),x)){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return;
+}
+
+int main()
+{
+    int t;
+    // cin>>t;
+    t=1;
+    while(t--)
+        solve();
+    return 0;
+}
