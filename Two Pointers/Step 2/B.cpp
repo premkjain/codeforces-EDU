b04976822730d1dce0520ac065504b6d9dd3b76b
Prem Kamal Jain <108341999+premkjain@users.noreply.github.com>
2022-10-23 12:49:43 +0530

    Create B.cpp

---
+++ Two Pointers/Step 2/B.cpp
@@ -0,0 +1,33 @@
+/* ____                       _  __                     _       _       _       
+  |  _ \ _ __ ___ _ __ ___   | |/ /__ _ _ __ ___   __ _| |     | | __ _(_)_ __  
+  | |_) | '__/ _ \ '_ ` _ \  | ' // _` | '_ ` _ \ / _` | |  _  | |/ _` | | '_ \ 
+  |  __/| | |  __/ | | | | | | . \ (_| | | | | | | (_| | | | |_| | (_| | | | | |
+  |_|   |_|  \___|_| |_| |_| |_|\_\__,_|_| |_| |_|\__,_|_|  \___/ \__,_|_|_| |_|
+*/                                                                            
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define hell    1000000007
+#define PI      3.141592653589793238462
+#define INF     1e18
+#define vi      vector<ll>
+#define pb      push_back
+#define ppb     pop_back
+#define mii     map<ll,ll>
+#define pii     pair<ll,ll>
+#define mp      make_pair
+#define ff      first
+#define ss      second
+#define all(x)  (x).begin(), (x).end()
+
+/*____________________________________________________________________________________________________________________________________________________________________________________________________*/
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
